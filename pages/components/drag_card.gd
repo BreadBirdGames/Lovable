e5eec986712f7adf5c67619680382c7680692a65
Frida Rosenaa <macrosedk@gmail.com>
2026-04-05 16:00:00 +0200

    Finish up drag card for now, add some "scoring tests"

--- pages/components/drag_card.gd
+++ pages/components/drag_card.gd
@@ -1,11 +1,17 @@
 class_name DragCard extends Control
 
+signal drag_switch(state: bool)
+signal dropped_on_left
+signal dropped_on_right
+
 var mouse_pos := Vector2.ZERO
 var start_mouse_pos := Vector2.ZERO
 var zero_pos := Vector2.ZERO
 var is_dragging := false
 
 @export var max_rotation := 0.5
+@export var max_position := 160
+@export var speed := 10.0
 
 func get_start_state() -> void:
 	zero_pos = position
@@ -19,21 +25,38 @@ func _process(delta: float) -> void:
 	if is_dragging:
 		var tilt_factor = clamp((mouse_pos.x - start_mouse_pos.x) / 200.0, -1.0, 1.0)
 		rotation = tilt_factor * max_rotation
-		position.x = lerp(position.x, (mouse_pos.x - pivot_offset.x), delta * 8)
+		position.x = lerp(
+			position.x, 
+			float(
+					clamp(
+						(mouse_pos.x - pivot_offset.x),
+						pivot_offset.x -max_position,
+						max_position
+					)
+				),
+				delta * speed
+			)
 	else:
-		position.x = lerp(position.x, zero_pos.x, delta * 8)
-		rotation = lerp(rotation, 0.0, delta * 8)
+		position.x = lerp(position.x, zero_pos.x, delta * speed)
+		rotation = lerp(rotation, 0.0, delta * speed)
 	
-
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.is_pressed():
 				if not is_dragging:  # Starting dragging
 					is_dragging = true
+					drag_switch.emit(is_dragging)
 					start_mouse_pos = event.global_position
 			else:
 				if is_dragging: # No longer dragging
 					is_dragging = false
-					
+					drag_switch.emit(is_dragging)
 					start_mouse_pos = Vector2.ZERO
+					
+					var viewport_width = get_viewport().get_visible_rect().size.x
+					if event.position.x < viewport_width/4:
+						dropped_on_left.emit()
+					elif event.position.x > viewport_width/2 + viewport_width/4:
+						dropped_on_right.emit()
+	
--- pages/home/home.gd
+++ pages/home/home.gd
@@ -1,4 +1,53 @@
 class_name Home extends Page
 
+@export var mark_tween_time: float = 0.25
+@export var left_mark: Control
+@export var right_mark: Control
+@export var counter: Label
+
+var count: int = 0
+var left_marks_tween: Tween
+var right_marks_tween: Tween
+
+func _ready() -> void:
+	left_mark.hide()
+	right_mark.hide()
+	
+	left_mark.modulate = Color.TRANSPARENT
+	right_mark.modulate = Color.TRANSPARENT
+
 #func _on_settings_button_pressed() -> void:
 #	PageManager.goto_page(Page.Type.SETTINGS)
+
+func _on_drag_card_dropped_on_left() -> void:
+	print("On left!")
+	count -= 1
+	counter.text = "SCORE: " + str(count)
+
+func _on_drag_card_dropped_on_right() -> void:
+	print("On right!")
+	count += 1
+	counter.text = "SCORE: " + str(count)
+
+func _on_drag_card_drag_switch(state: bool) -> void:
+	print(state)
+	if left_marks_tween:
+		left_marks_tween.kill()
+	left_marks_tween = get_tree().create_tween()
+	
+	if right_marks_tween:
+		right_marks_tween.kill()
+	right_marks_tween = get_tree().create_tween()
+	
+	if state: # We're hovering
+		left_mark.show()
+		right_mark.show()
+	
+		left_marks_tween.tween_property(left_mark, "modulate", Color.WHITE, mark_tween_time)
+		right_marks_tween.tween_property(right_mark, "modulate", Color.WHITE, mark_tween_time)
+	else:
+		left_marks_tween.tween_property(left_mark, "modulate", Color.TRANSPARENT, mark_tween_time)
+		right_marks_tween.tween_property(right_mark, "modulate", Color.TRANSPARENT, mark_tween_time)
+		
+		left_marks_tween.tween_callback(left_mark.hide)
+		right_marks_tween.tween_callback(right_mark.hide)
